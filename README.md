7893e42c1d3c701c7debf0981956595b45988c86
gggmmm <giovanni.meciani@gmail.com>
2019-05-13 21:59:22 +0200

    Two windows.

--- README.md
+++ README.md
@@ -5,19 +5,19 @@ To process the image, 'convert' by ImageMagick is used.
 
 ![alt text](screen.png "App screenshot")
 
+In the display window:
 + First line: what pytesseract is reading from the second image.
 + Second line: original image.
 + Third line: processed image with the following parameters (read by pytesseract).
+
+In the controls window you can set the following options that affect the 'convert' function:
 + Set '-resize' %. 100= unchanged, 1000=10 times bigger.
 + Set '-threshold' in %.
 + Set brightness in '-brightness-contrast'.
 + Set contrast in '-brightness-contrast'.
 + Set border '-border'. E.g. 3= a 3pixel by 3pixel white border. This one is hard to see in the app, but check the output file to see it better.
 
-Usage
-```bash
-python3 main.py
-```
+Usage: first place a file called 'test.png' in the same folder as the program, then do `python3 main.py`.
 
 Suggestion: the slider that controls the size (resize) is best used when clicking on the dark area. Instead, if you slide by grabbing the slider the interface gets messy.
 
@@ -28,3 +28,11 @@ Tested with:
 - pytesseract 0.2.6
 - TkInter 8.6
 - ImageMagick 6.9.7-4 Q16
+
+Coming soon (in order of priority/feasibility):
+. save current settings/load last saved settings
+. show the full 'convert' function with all used options
+. enter parameters through text-box (to avoid slow  slider)
+. personalised slider-step
+. ordering of the options. Currently the order is from top to bottom, but doing thresholding before resizing may make a difference
+. checkbox to enable/disable each slider
--- main.py
+++ main.py
@@ -4,190 +4,212 @@ import time
 from PIL import Image, ImageTk
 import pytesseract
 
-size = 100
-threshold = 50
-brightness = 0
-contrast = 0
-NUM_OF_ITERATIONS = 10
-last_command = []
-input_dir = './'
-input_file = 'test.png'
-infi = input_dir+input_file
-output_dir = './'
+input_dir   = './'
+input_file  = 'test.png'
+infi        = input_dir+input_file
+output_dir  = './'
 output_file = 'test_output.png'
-outfi = output_dir+output_file
-border = '3x3'
-bordercolor = 'White'
+outfi       = output_dir+output_file
 
-assert infi != outfi
+class Controls(tk.Frame):
+        theOtherWindow  = None        
+        master          = None
+        
+        size                = 100
+        threshold           = 50
+        brightness          = 0
+        contrast            = 0
+        NUM_OF_ITERATIONS   = 10
+        last_command        = []
+        
+        border      = '3x3'
+        bordercolor = 'White'
+        
+        def __init__(self, master=None):
+            super().__init__(master)
+            
+            self.master = master
+            
+            # ======== SCALES ========
+            lab_sre = tk.Label(master, name='lab_sre', text='Resize').grid(row=3, column=0, sticky=tk.W)
 
-def updateImage():
-    global w
-    global size
-    global threshold
-    global brightness
-    global contrast
-    global border
-    global bordercolor
-    global last_command
+            sre = tk.Scale(master, name='sre', orient=tk.HORIZONTAL, length=300, resolution=25, from_=100, to=1000, tickinterval=900, command=self.updateSize).grid(row=3, column=1)
+            h = master.nametowidget('sre')
+            h.set(100)
+
+
+            lab_sth = tk.Label(master, name='lab_sth', text='Threshold').grid(row=4, column=0, sticky=tk.W)
+
+            sth = tk.Scale(master, name='sth', orient=tk.HORIZONTAL, length=300, resolution=1, from_=0, to=100, tickinterval=100, command=self.updateThreshold).grid(row=4, column=1)
+            h = master.nametowidget('sth')
+            h.set(85)
+
+
+            lab_sbr = tk.Label(master, name='lab_sbr', text='Brightness').grid(row=5, column=0, sticky=tk.W)
+
+            sbr = tk.Scale(master, name='sbr', orient=tk.HORIZONTAL, length=300, resolution=1, from_=-100, to=100, tickinterval=100, command=self.updateBrightness).grid(row=5, column=1)
+            h = master.nametowidget('sbr')
+            h.set(-42)
+
+
+            lab_sco = tk.Label(master, name='lab_sco', text='Contrast').grid(row=6, column=0, sticky=tk.W)
+
+            sco = tk.Scale(master, name='sco', orient=tk.HORIZONTAL, length=300, resolution=1, from_=-100, to=100, tickinterval=100, command=self.updateContrast).grid(row=6, column=1)
+            h = master.nametowidget('sco')
+            h.set(100)
+
+
+            lab_sbo = tk.Label(master, name='lab_sbo', text='Border size').grid(row=7, column=0, sticky=tk.W)
+
+            sbo = tk.Scale(master, name='sbo', orient=tk.HORIZONTAL, length=300, resolution=1, from_=0, to=10, tickinterval=10, command=self.updateBorder).grid(row=7, column=1)
+            h = master.nametowidget('sbo')
+            h.set(3)
+
+            # ====== /SCALES ======
+            
+            check_timings_btn = tk.Button(master, text='Check timings with following\n amount of iterations', name='check_timings_btn', command=self.check_timings).grid(row=8, column=0)
+
+            num_of_iterations = tk.Text(master, name='num_of_iterations_text', width=4, height=1).grid(row=8, column=1, sticky=tk.W)
+            h = master.nametowidget('num_of_iterations_text')
+            h.insert(tk.END, str(self.NUM_OF_ITERATIONS))
+            
+            # time to execute pytesseract image_to_string, i.e. read the image
+            lab_exec_pytess = tk.Label(master, name='lab_exec_pytess', text='Exec. time pytesseract [s]').grid(row=9, column=0, sticky=tk.W)
+            exectime_pytess = tk.Label(master, text='', name='exectime_pytess', font='Helvetica 16').grid(row=9, column=1)
+
+            # time to invoke the 'convert' function and execute it
+            lab_exec_conv = tk.Label(master, name='lab_exec_conv', text='Exec. time convert [s]').grid(row=10, column=0, sticky=tk.W)
+            exectime_convert = tk.Label(master, text='', name='exectime_convert', font='Helvetica 16').grid(row=10, column=1)
+            
+        def setTheOtherWindow(self, tow):
+            self.theOtherWindow = tow
+                
+        def updateThreshold(self, value):
+            self.threshold = value
+            self.updateImage()
+            
+        def updateSize(self, value):
+            self.size = value
+            self.updateImage()
+            
+        def updateContrast(self, value):
+            self.contrast = value
+            self.updateImage()
+
+        def updateBrightness(self, value):
+            self.brightness = value
+            self.updateImage()
+
+        def updateBorder(self, value):
+            self.border = str(value)+'x'+str(value)
+            self.updateImage()
+            
+        def updateImage(self):
+            global infi
+            global outfi
+        
+            s0 = time.time()
+            self.last_command = ['convert', infi, '-resize', str(self.size)+'%', '-brightness-contrast', str(self.brightness)+'x'+str(self.contrast), '-threshold', str(self.threshold)+'%', '-border',self.border,'-bordercolor', self.bordercolor, outfi]
+            sp.call(self.last_command)
+            
+            s1 = time.time()
+            
+            read = pytesseract.image_to_string(Image.open(outfi))
+            s2 = time.time()
+            
+            
+            h = self.theOtherWindow.nametowidget('tesseract')
+            h.config(text=read)
+            
+            h = self.master.nametowidget('exectime_convert')
+            h.config(text=str(s1-s0))
+            
+            h = self.master.nametowidget('exectime_pytess')
+            h.config(text=str(s2-s1))
+            
+            # redraw update image
+            # destroy old
+            h = self.theOtherWindow.nametowidget('img_label')
+            h.destroy()
+            
+            #create new
+            self.img2 = tk.PhotoImage(file=outfi)
+            l = tk.Label(self.theOtherWindow, image=self.img2, name='img_label').grid(row=2)
+        
+        def check_timings(self):
+            global outfi
+
+            h = self.master.nametowidget('num_of_iterations_text')
+            self.NUM_OF_ITERATIONS = int(h.get('current linestart', tk.END))
+
+            avg_convert = 0
+            avg_tesseract = 0
+            for i in range(self.NUM_OF_ITERATIONS):
+                s = time.time()
+                sp.call(self.last_command)
+                
+                s1 = time.time()
+                
+                read = pytesseract.image_to_string(Image.open(outfi))
+                s2 = time.time()
+                
+                avg_convert += s1-s
+                avg_tesseract += s2-s1
+
+            avg_convert     /= self.NUM_OF_ITERATIONS
+            avg_tesseract   /= self.NUM_OF_ITERATIONS
+
+            h = self.master.nametowidget('exectime_convert')
+            h.config(text=str(avg_convert))
+
+            h = self.master.nametowidget('exectime_pytess')
+            h.config(text=str(avg_tesseract))
+        
+class Display(tk.Frame):
+        img = ''
+        img2 = ''
+        master = None
+        
+        def __init__(self, master=None):
+            super().__init__(master)
+            
+            self.master = master
+            
+            # pytesseract output
+            tesseract = tk.Label(master, text='', name='tesseract', font='Helvetica 16').grid(row=0)
+            
+            global infi
+            global outfi
+            
+            self.img = tk.PhotoImage(file=infi)
+            l = tk.Label(master, image=self.img).grid(row=1)
+
+            self.img2 = tk.PhotoImage(file=outfi)
+            l2 = tk.Label(master, image=self.img2, name='img_label').grid(row=2)
+    
+def main():    
+    root = tk.Tk()
+    root.title('Controls - Pytesseract interactive filtering')
+    controls = Controls(root)
+    
+    # get screen width and height
+    ws = root.winfo_screenwidth() # width of the screen
+    hs = root.winfo_screenheight() # height of the screen
+    root.geometry('+150+150') #str(int(ws/2))+'x'+str(int(hs))+'+0+0')
+
+    second_win = tk.Toplevel(root)
+    second_win.title('Display - Pytesseract interactive filtering')
+    second_win.geometry('+650+150')#str(int(ws/2))+'x'+str(int(hs))+'+'+str(int(ws/2))+'+0')
+    d = Display(second_win)
+    
+    controls.setTheOtherWindow(second_win)
+    
+    # just to create an output file immediately
     global infi
     global outfi
-    
-    s0 = time.time()
-    last_command = ['convert', infi, '-resize', str(size)+'%', '-brightness-contrast', str(brightness)+'x'+str(contrast), '-threshold', str(threshold)+'%', '-border',border,'-bordercolor', bordercolor, outfi]
-    sp.call(last_command)
-    
-    s1 = time.time()
-    
-    read = pytesseract.image_to_string(Image.open(outfi))
-    s2 = time.time()
-    
-    
-    h = w.nametowidget('tesseract')
-    h.config(text=read)
-    
-    h = w.nametowidget('exectime_convert')
-    h.config(text=str(s1-s0))
-    
-    h = w.nametowidget('exectime_pytess')
-    h.config(text=str(s2-s1))
-    
-    # redraw update image
-    # destroy old
-    h = w.nametowidget('img_label')
-    h.destroy()
-    
-    #create new
-    global img2
-    img2 = tk.PhotoImage(file=outfi)
-    l = tk.Label(w, image=img2, name='img_label').grid(row=2, columnspan=2)
-
-def updateThreshold(value):
-    global threshold
-    threshold = value
-    updateImage()
-    
-def updateSize(value):
-    global size
-    size = value
-    updateImage()
-    
-def updateContrast(value):
-    global contrast
-    contrast = value
-    updateImage()
-
-def updateBrightness(value):
-    global brightness
-    brightness = value
-    updateImage()
-
-def updateBorder(value):
-    global border
-    border = str(value)+'x'+str(value)
-    updateImage()
-
-def check_timings():
-    global w
-    global NUM_OF_ITERATIONS
-    global last_command
-    global outfi
-    
-    h = w.nametowidget('num_of_iterations_text')
-    NUM_OF_ITERATIONS = int(h.get('current linestart', tk.END))
-    
-    avg_convert = 0
-    avg_tesseract = 0
-    for i in range(NUM_OF_ITERATIONS):
-        s = time.time()
-        sp.call(last_command)
-        
-        s1 = time.time()
-        
-        read = pytesseract.image_to_string(Image.open(outfi))
-        s2 = time.time()
-        
-        avg_convert += s1-s
-        avg_tesseract += s2-s1
-    
-    avg_convert     /= NUM_OF_ITERATIONS
-    avg_tesseract   /= NUM_OF_ITERATIONS
-    
-    h = w.nametowidget('exectime_convert')
-    h.config(text=str(avg_convert))
-    
-    h = w.nametowidget('exectime_pytess')
-    h.config(text=str(avg_tesseract))    
-
-def main():
-    global w
-    
-    w = tk.Tk()
-    w.title('Pytesseract interactive filtering')
-
     sp.call(['convert', infi, outfi])
-
-    # pytesseract output
-    tesseract = tk.Label(w, text='', name='tesseract', font='Helvetica 16').grid(row=0, columnspan=2)
-
-    img = tk.PhotoImage(file=infi)
-    l = tk.Label(w, image=img).grid(row=1, columnspan=2)
-
-    img2 = tk.PhotoImage(file=outfi)
-    l2 = tk.Label(w, image=img2, name='img_label').grid(row=2, columnspan=2)
-
-    # ======== SCALES ========
-    lab_sre = tk.Label(w, name='lab_sre', text='Resize').grid(row=3, column=0, sticky=tk.W)
-
-    sre = tk.Scale(w, name='sre', orient=tk.HORIZONTAL, length=300, resolution=25, from_=100, to=1000, tickinterval=900, command=updateSize).grid(row=3, column=1)
-    h = w.nametowidget('sre')
-    h.set(200)
-
-
-    lab_sth = tk.Label(w, name='lab_sth', text='Threshold').grid(row=4, column=0, sticky=tk.W)
-
-    sth = tk.Scale(w, name='sth', orient=tk.HORIZONTAL, length=300, resolution=1, from_=0, to=100, tickinterval=100, command=updateThreshold).grid(row=4, column=1)
-    h = w.nametowidget('sth')
-    h.set(85)
-
-
-    lab_sbr = tk.Label(w, name='lab_sbr', text='Brightness').grid(row=5, column=0, sticky=tk.W)
-
-    sbr = tk.Scale(w, name='sbr', orient=tk.HORIZONTAL, length=300, resolution=1, from_=-100, to=100, tickinterval=100, command=updateBrightness).grid(row=5, column=1)
-    h = w.nametowidget('sbr')
-    h.set(-42)
-
-
-    lab_sco = tk.Label(w, name='lab_sco', text='Contrast').grid(row=6, column=0, sticky=tk.W)
-
-    sco = tk.Scale(w, name='sco', orient=tk.HORIZONTAL, length=300, resolution=1, from_=-100, to=100, tickinterval=100, command=updateContrast).grid(row=6, column=1)
-    h = w.nametowidget('sco')
-    h.set(100)
-
-
-    lab_sbo = tk.Label(w, name='lab_sbo', text='Border size').grid(row=7, column=0, sticky=tk.W)
-
-    sbo = tk.Scale(w, name='sbo', orient=tk.HORIZONTAL, length=300, resolution=1, from_=0, to=10, tickinterval=10, command=updateBorder).grid(row=7, column=1)
-    h = w.nametowidget('sbo')
-    h.set(3)
-
-    # ====== /SCALES ======
     
-    check_timings_btn = tk.Button(w, text='Check timings with following\n amount of iterations', name='check_timings_btn', command=check_timings).grid(row=8, column=0)
-
-    num_of_iterations = tk.Text(w, name='num_of_iterations_text', width=4, height=1).grid(row=8, column=1, sticky=tk.W)
-    h = w.nametowidget('num_of_iterations_text')
-    h.insert(tk.END, str(NUM_OF_ITERATIONS))
-    
-    # time to execute pytesseract image_to_string, i.e. read the image
-    lab_exec_pytess = tk.Label(w, name='lab_exec_pytess', text='Exec. time pytesseract [s]').grid(row=9, column=0, sticky=tk.W)
-    exectime_pytess = tk.Label(w, text='', name='exectime_pytess', font='Helvetica 16').grid(row=9, column=1)
-
-    # time to invoke the 'convert' function and execute it
-    lab_exec_conv = tk.Label(w, name='lab_exec_conv', text='Exec. time convert [s]').grid(row=10, column=0, sticky=tk.W)
-    exectime_convert = tk.Label(w, text='', name='exectime_convert', font='Helvetica 16').grid(row=10, column=1)
-        
-    w.mainloop()
+    root.mainloop()
     
 if __name__ == '__main__':
     main()
